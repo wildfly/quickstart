8be19132d5dc5fb48091a5038c8043aa93364dd2
Pete Muir <pmuir@bleepbleep.org.uk>
2011-05-25 13:37:02 +0100

    Initial import of numberguess for AS7

---
+++ quickstarts/numberguess/src/main/java/org/jboss/weld/examples/numberguess/MaxNumber.java
@@ -0,0 +1,23 @@
+package org.jboss.weld.examples.numberguess;
+
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+@Target( { TYPE, METHOD, PARAMETER, FIELD })
+@Retention(RUNTIME)
+@Documented
+@Qualifier
+public @interface MaxNumber
+{
+
+}
