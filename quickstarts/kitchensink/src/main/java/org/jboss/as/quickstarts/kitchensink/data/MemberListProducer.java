4d81f50ae9436ffd5c75f6e1c8eab88b4fff5c20
Pete Muir <pmuir@bleepbleep.org.uk>
2011-06-24 13:34:00 +0100

    Various fixes from Vimal Kansal + tech overview

--- quickstarts/kitchensink/src/main/java/org/jboss/as/quickstarts/kitchensink/data/MemberListProducer.java
+++ quickstarts/kitchensink/src/main/java/org/jboss/as/quickstarts/kitchensink/data/MemberListProducer.java
@@ -22,7 +22,7 @@ public class MemberListProducer {
 
     private List<Member> members;
 
-    // @Named provides access the return value via the EL variable name "member" in the UI (e.g., Facelets or JSP view)
+    // @Named provides access the return value via the EL variable name "members" in the UI (e.g., Facelets or JSP view)
     @Produces
     @Named
     public List<Member> getMembers() {
