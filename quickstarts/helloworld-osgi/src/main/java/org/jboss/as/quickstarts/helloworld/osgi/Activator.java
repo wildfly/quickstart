60988e380f7b1d24441cbededab6580318dcd3d2
Pete Muir <pmuir@bleepbleep.org.uk>
2011-06-27 16:20:31 +0100

    Whitespace

--- quickstarts/helloworld-osgi/src/main/java/org/jboss/as/quickstarts/helloworld/osgi/Activator.java
+++ quickstarts/helloworld-osgi/src/main/java/org/jboss/as/quickstarts/helloworld/osgi/Activator.java
@@ -25,11 +25,11 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
-    public void start(BundleContext context) throws Exception {
-        System.out.println("Hello AS7!");
-    }
+   public void start(BundleContext context) throws Exception {
+      System.out.println("Hello AS7!");
+   }
 
-    public void stop(BundleContext context) throws Exception {
-        System.out.println("Bye AS7!");
-    }
+   public void stop(BundleContext context) throws Exception {
+      System.out.println("Bye AS7!");
+   }
 }
