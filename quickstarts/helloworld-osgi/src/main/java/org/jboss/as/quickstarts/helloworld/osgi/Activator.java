6841f1f122a5c70a044f04bbb09aa16d1541e77b
David Bosschaert <david.bosschaert@gmail.com>
2011-06-28 15:07:42 +0100

    Minor reformatting

--- quickstarts/helloworld-osgi/src/main/java/org/jboss/as/quickstarts/helloworld/osgi/Activator.java
+++ quickstarts/helloworld-osgi/src/main/java/org/jboss/as/quickstarts/helloworld/osgi/Activator.java
@@ -26,10 +26,10 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
    public void start(BundleContext context) throws Exception {
-      System.out.println("Hello AS7!");
+      System.out.println("Hello AS7 World!!");
    }
 
    public void stop(BundleContext context) throws Exception {
-      System.out.println("Bye AS7!");
+      System.out.println("Bye AS7 World!!");
    }
 }
