5d39f226011bc71bdaabb1efac0490fe1609a979
Gustavo Andres Brey <gusbrey@gmail.com>
2011-11-02 16:25:30 -0300

    Fix JSON format

--- rs-helloworld/src/main/java/org/jboss/as/quickstarts/rshelloworld/HelloWorld.java
+++ rs-helloworld/src/main/java/org/jboss/as/quickstarts/rshelloworld/HelloWorld.java
@@ -23,7 +23,7 @@ public class HelloWorld {
 	@Path("/json")
 	@Produces({ "application/json" })
 	public String getHelloWorldJSON() {
-		return "{result:\"" + helloService.createHelloMessage("World") + "\"}";
+		return "{\"result\":\"" + helloService.createHelloMessage("World") + "\"}";
 	}
 
 	@GET
