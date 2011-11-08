6a5aba0a07a994901fa453152d5a55e8fcfeea54
Sande Gilda <sgilda@redhat.com>
2011-11-08 09:53:35 -0500

    Modified the REST path to use Member instead of Registrant

--- kitchensink-ear/jboss-as-kitchensink-ear-web/src/main/java/org/jboss/as/quickstarts/rest/MemberResourceRESTService.java
+++ kitchensink-ear/jboss-as-kitchensink-ear-web/src/main/java/org/jboss/as/quickstarts/rest/MemberResourceRESTService.java
@@ -17,7 +17,7 @@ import org.jboss.as.quickstarts.model.Member;
  * 
  * This class produces a RESTful service to read the contents of the members table.
  */
-@Path("/registrants")
+@Path("/members")
 @RequestScoped
 public class MemberResourceRESTService {
    @Inject
