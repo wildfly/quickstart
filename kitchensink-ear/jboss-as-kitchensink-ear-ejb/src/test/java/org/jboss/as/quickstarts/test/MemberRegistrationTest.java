617298ad2edc97e3c1509ee404ff30f2c83b6564
Sande Gilda <sgilda@redhat.com>
2011-11-29 15:49:46 -0500

    New enterprise version of kitchensink-ear: EAR

---
+++ kitchensink-ear/jboss-as-kitchensink-ear-ejb/src/test/java/org/jboss/as/quickstarts/test/MemberRegistrationTest.java
@@ -0,0 +1,49 @@
+package org.jboss.as.quickstarts.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.jboss.as.quickstarts.controller.MemberRegistration;
+import org.jboss.as.quickstarts.model.Member;
+import org.jboss.as.quickstarts.util.Resources;
+
+@RunWith(Arquillian.class)
+public class MemberRegistrationTest {
+   @Deployment
+   public static Archive<?> createTestArchive() {
+      return ShrinkWrap.create(WebArchive.class, "test.war")
+            .addClasses(Member.class, MemberRegistration.class, Resources.class)
+            .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+   }
+
+   @Inject
+   MemberRegistration memberRegistration;
+
+   @Inject
+   Logger log;
+
+   @Test
+   public void testRegister() throws Exception {
+      Member newMember = memberRegistration.getNewMember();
+      newMember.setName("Jane Doe");
+      newMember.setEmail("jane@mailinator.com");
+      newMember.setPhoneNumber("2125551234");
+      memberRegistration.register();
+      assertNotNull(newMember.getId());
+      log.info(newMember.getName() + " was persisted with id " + newMember.getId());
+   }
+   
+}
