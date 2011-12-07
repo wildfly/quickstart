4347086a3a48575e4b71c4cc83b86d6dedf6923a
Weston M. Price <wprice@redhat.com>
2011-12-07 10:34:20 -0500

    JMS quickstart example

---
+++ helloworld-jms/README.md
@@ -0,0 +1,59 @@
+HelloWorld JMS Example
+======================
+This example demonstrates the use of external JMS clients with JBoss7.
+
+The example provides a demonstration of both producing and consuming messages to and from
+a JMS destination deployed in the JBoss7 environment. The example can be run from the Maven
+commandline, or from the Eclipse environment. 
+
+To run the example from the commandline simply type:
+
+mvn compile
+mvn exec:java
+
+The example provides for a certain amount of customization for the mvn:exec plugin via system properties explained below:
+
+Required Properties
+===================
+connection.type
+	Which connection type to the JMS provider you want to use. Accepted values are netty or jndi.
+	Default: netty
+	Note**
+		Currently external JNDI access is under development in JBoss7. As a result netty is the default value.
+		
+java.naming.factory.initial
+	The JNDI intial context factory you wish to use
+	Default: org.jboss.as.naming.InitialContextFactory
+
+java.naming.provider.url
+	The provider URL of the JNDI context you wish to use
+	Default: jnp://localhost:1099
+
+Optional Properties
+===================
+cf.name
+	The JNDI name of the JMS ConnectionFactory you want to use.
+	Default: RemoteConnectionFactory
+
+dest.name
+	The JNDI name of the JMS Destination you want to use.
+	Default testQueue
+
+message.content
+	The content of the JMS TextMessage.
+	Default: Hello, World!
+
+message.count
+	The number of JMS messages you want to produce and consume.
+	Default 1
+	
+**Deployment Note**
+By default, the JMS messaging provider is not deployed with the standalone JBoss7 server. You will either need to run
+a domain server, or configure the standalone server for JMS message. Please refer to the JBoss7 Documentation for further
+details.
+
+
+	
+
+
+ 
---
+++ helloworld-jms/src/main/java/org/jboss/as/quickstarts/jms/HelloWorldJMSClient.java
@@ -0,0 +1,71 @@
+package org.jboss.as.quickstarts.jms;
+
+import java.util.logging.Logger;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+public class HelloWorldJMSClient
+{
+    private static final Logger log = Logger.getLogger(HelloWorldJMSClient.class.getName());
+
+    private static final String DEFAULT_MESSAGE = "Hello, World!";
+    private static final int DEFAULT_MESSAGE_COUNT = 1;
+
+    public static void main(String[] args)
+    {
+
+        ConnectionFactory connectionFactory = null;
+        Connection connection = null;
+        Session session = null;
+        MessageProducer producer = null;
+        MessageConsumer consumer = null;
+        Destination destination = null;
+        TextMessage message = null;
+
+        try
+        {
+            connectionFactory = JMSClientUtil.getConnectionFactory();
+            destination = JMSClientUtil.getDestination();
+            connection = connectionFactory.createConnection();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            producer = session.createProducer(destination);
+            consumer = session.createConsumer(destination);
+            connection.start();
+            
+            int count = (System.getProperty("message.count") == null) ? DEFAULT_MESSAGE_COUNT : Integer.valueOf(System.getProperty("message.count"));            
+            String content = (System.getProperty("message.content") == null) ? DEFAULT_MESSAGE : System.getProperty("message.content");
+            
+            log.info("Sending " + count +  " messages with content: " + content);
+            
+            for(int i = 0; i < count; i++)
+            {
+                message = session.createTextMessage();
+                message.setText(content);
+                producer.send(message);
+
+            }            
+            
+            for(int i = 0; i < count; i++)
+            {
+                message = (TextMessage)consumer.receive();
+                log.info("Received message with content " + message.getText());
+            }
+        }
+        catch(Exception e)
+        {
+            log.severe(e.getMessage());
+        }
+        finally
+        {
+            JMSClientUtil.closeResources(producer, consumer, session, connection);
+        }
+
+    }
+
+}
