c2d63703810068f0e41c31072fb6dadbb4c9d1b5
Jay Balunas <tech4j@gmail.com>
2012-01-05 11:26:49 -0500

    aerogear-46 added unique table name

--- html5-mobile/src/main/java/org/jboss/as/quickstarts/html5_mobile/model/Member.java
+++ html5-mobile/src/main/java/org/jboss/as/quickstarts/html5_mobile/model/Member.java
@@ -19,7 +19,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @XmlRootElement
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@Table(name = "MemberHTML5", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class Member implements Serializable {
    /** Default value included to remove warning. Remove or modify at will. **/
    private static final long serialVersionUID = 1L;
