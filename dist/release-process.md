48fe3ca87d8dfd77ae92cec6b8c7cc72481ec307
Pete Muir <pmuir@bleepbleep.org.uk>
2012-02-21 15:32:35 +0000

    Add release.sh script, remove redundant README

--- dist/release-process.md
+++ dist/release-process.md
@@ -1,42 +1,16 @@
 Releasing
 =========
 
-1. Make sure you have credentials set up to deploy to `jboss-releases-repository` in your `settings.xml`
+1. Make sure you have credentials set up to deploy to `jboss-releases-repository` in your `settings.xml`, and have access to rsync files to `filemgmt.jboss.org/download_htdocs/jbossas`
 2. Release the archetypes
-3. Regenerate the quickstart based archetypes
+3. Regenerate the quickstart based on archetypes
 
         ./dist/release-utils.sh -r
 
-4. Regenerate html readmes from markdown
+4. Release
+
+        dist/release.sh -s <old snapshot version> -r <release version>
+
+5. Log in to the Nexus interface, and close / release 
         
-         ./dist/release-utils.sh -m
-5. Commit this
-6. Update the version numbers
-
-        dist/release-utils.sh -u -o <old snapshot version> -n <release version>
-
-7. Commit this
-8. Tag using
-        
-        git tag -a <release version> -m "Tag <release version>"
-
-9. Reset the version numbers for development
-
-        dist/release-utils.sh -u -n <new snapshot version> -o <release version>
-
-10. Check out the tag
-
-        git checkout <release version>
-        
-11. Deploy to Maven
-
-        mvn clean deploy -DaltDeploymentRepository=jboss-releases-repository::default::https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/
-        
-12. Log in to the Nexus interface, and release 
-13. Build the distro
-
-        mvn clean install -f dist/pom.xml
-14. Rsync the zip to download.jboss.org
-
-        rsync -Pv --protocol=28 jbossas@download.jboss.org:download_htdocs/jbossas/7.<minor version>/jboss-as-<version>
-15. Update the jboss.org/jbossas/downloads magnolia page
+6. Update the jboss.org/jbossas/downloads magnolia page
---
+++ dist/release.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# DEFINE
+
+VERSION_REGEX='([0-9]*)\.([0-9]*)([a-zA-Z0-9\.]*)'
+
+
+# SCRIPT
+
+usage()
+{
+cat << EOF
+usage: $0 options
+
+This script performs a release of the Quickstarts 
+
+OPTIONS:
+   -s      Snapshot version number to update from
+   -n      New snapshot version number to update to, if undefined, defaults to the version number updated from
+   -r      Release version number
+EOF
+}
+
+release()
+{
+   echo "Regenerating html from markdown"
+   $DIR/release-utils.sh -m
+   echo "Releasing JBoss AS Quickstarts version $RELEASEVERSION"
+   $DIR/release-utils.sh -u -o $SNAPSHOTVERSION -n $RELEASEVERSION
+   git commit -a -m "Prepare for $RELEASEVERSION release"
+   git tag -a $RELEASEVERSION -m "Tag $RELEASEVERSION"
+   $DIR/release-utils.sh -u -o $RELEASEVERSION -n $NEWSNAPSHOTVERSION
+   git commit -a -m "Prepare for development of $NEWSNAPSHOTVERSION"
+   echo "Building Distribution zip"
+   BRANCH=$(parse_git_branch)
+   git checkout $RELEASEVERSION
+   mvn clean install -f $DIR/pom.xml
+   git checkout $BRANCH
+   rsync -Pv --protocol=28 $DIR/target/jboss-as-quickstarts-$RELEASEVERSION-dist.zip jbossas@filemgmt.jboss.org:downloads_htdocs/jbossas/$MAJOR_VERSION.$MINOR_VERSION/jboss-as-$RELEASEVERSION
+}
+
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+}
+
+
+SNAPSHOTVERSION="UNDEFINED"
+RELEASEVERSION="UNDEFINED"
+NEWSNAPSHOTVERSION="UNDEFINED"
+MAJOR_VERSION="UNDEFINED"
+MINOR_VERSION="UNDEFINED"
+
+while getopts “n:r:s:” OPTION
+
+do
+     case $OPTION in
+         h)
+             usage
+             exit
+             ;;
+         s)
+             SNAPSHOTVERSION=$OPTARG
+             ;;
+         r)
+             RELEASEVERSION=$OPTARG
+             ;;
+         n)
+             NEWSNAPSHOTVERSION=$OPTARG
+             ;;
+         [?])
+             usage
+             exit
+             ;;
+     esac
+done
+
+if [[ $RELEASEVERSION =~ $VERSION_REGEX ]]; then
+   MAJOR_VERSION=${BASH_REMATCH[1]}
+   MINOR_VERSION=${BASH_REMATCH[2]}
+fi
+
+if [ "$NEWSNAPSHOTVERSION" == "UNDEFINED" ]
+then
+   NEWSNAPSHOTVERSION=$SNAPSHOTVERSION
+fi
+
+if [ "$MAJOR_VERSION" == "UNDEFINED" -o  "$MINOR_VERSION" == "UNDEFINED" ]
+then
+   echo "\nUnable to extract major and minor versions\n"
+   usage
+   exit
+fi
+
+if [ "$SNAPSHOTVERSION" == "UNDEFINED" -o  "$RELEASEVERSION" == "UNDEFINED" ]
+then
+   echo "\nMust specify -r and -s\n"
+   usage
+   exit
+fi
+
+release
+
