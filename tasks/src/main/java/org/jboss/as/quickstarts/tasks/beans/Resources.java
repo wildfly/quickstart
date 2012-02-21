445501a6dff26a639761dc739f13ed8413a3f4de
Pete Muir <pmuir@bleepbleep.org.uk>
2012-02-21 21:28:20 +0000

    Make Resources request scoped

--- tasks/src/main/java/org/jboss/as/quickstarts/tasks/beans/Resources.java
+++ tasks/src/main/java/org/jboss/as/quickstarts/tasks/beans/Resources.java
@@ -1,12 +1,14 @@
 package org.jboss.as.quickstarts.tasks.beans;
 
 import javax.ejb.Stateful;
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
 
 @Stateful
+@RequestScoped
 public class Resources {
 
    @PersistenceContext(type=PersistenceContextType.EXTENDED)
