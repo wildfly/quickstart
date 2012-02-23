678ee35140acc5f9dc8bb4a533078c01dc6a9f88
kborchers <kris.borchers@gmail.com>
2012-02-23 14:19:24 -0600

    whitespace

--- kitchensink-html5-mobile/client/src/test/qunit/test/test.js
+++ kitchensink-html5-mobile/client/src/test/qunit/test/test.js
@@ -21,35 +21,35 @@ Unit tests that cover basic functionality of app.js.
 module('Member Row Construction');
 
 test('Build 2 Member Rows', function() {
-   expect(1);
+    expect(1);
 
-   var members = [{"email": "jane.doe@company.com", "id": 1, "name": "Jane Doe", "phoneNumber": "12312312311"},{"email": "john.doe@company.com", "id": 0, "name": "John Doe", "phoneNumber": "2125551212"}];
+    var members = [{"email": "jane.doe@company.com", "id": 1, "name": "Jane Doe", "phoneNumber": "12312312311"},{"email": "john.doe@company.com", "id": 0, "name": "John Doe", "phoneNumber": "2125551212"}];
 
-   var html = buildMemberRows(members);
-   var length = $(html).length;
-   ok(length == 2, 'Number of rows built: ' + length);
+    var html = buildMemberRows(members);
+    var length = $(html).length;
+    ok(length == 2, 'Number of rows built: ' + length);
 });
 
 test('Build 0 member Rows', function() {
-  expect(1);
+    expect(1);
 
-   var members = [];
+    var members = [];
 
-   var html = buildMemberRows(members);
+    var html = buildMemberRows(members);
 
-   ok(html == '', 'Created no rows for empty members');
+    ok(html == '', 'Created no rows for empty members');
 });
 
 module('Member Restful Calls');
 
 test('Request current member list', function() {
-  ok(1==1,"TODO");
+    ok(1==1,"TODO");
 });
 
 test('Register a new member', function() {
-  ok(1==1,"TODO");
+    ok(1==1,"TODO");
 });
 
 test('Register a member with a duplicate email', function() {
-  ok(1==1,"TODO");
+    ok(1==1,"TODO");
 });
