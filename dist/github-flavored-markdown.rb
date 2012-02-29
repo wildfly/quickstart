8dd4a28493d2d983de5c698377feeace84dbae37
Pete Muir <pmuir@bleepbleep.org.uk>
2012-02-29 14:41:30 +0000

    remove syntax highlight

--- dist/github-flavored-markdown.rb
+++ dist/github-flavored-markdown.rb
@@ -26,20 +26,9 @@
 
 require 'rubygems'
 require 'redcarpet'
-require 'albino'
-
-class SyntaxRenderer < Redcarpet::Render::HTML
-  def block_code(code, language)
-    if language && !language.empty?
-      Albino.colorize(code, language)
-    else
-      "<pre><code>#{code}</code></pre>"
-    end
-  end
-end
 
 def markdown(text)
-  renderer = SyntaxRenderer.new(optionize [
+  renderer = Redcarpet::Render::HTML.new(optionize [
     :with_toc_data,
     :hard_wrap,
     :xhtml
