a9aa3c68389b86be809b1db96e9f524b1782079d
Justin Bertram <jbertram@redhat.com>
2012-03-13 16:37:57 -0500

    Major re-work of JMS quickstart

--- helloworld-jms/README.md
+++ helloworld-jms/README.md
@@ -6,74 +6,79 @@ The example provides a demonstration of both producing and consuming messages to
 a JMS destination deployed in the JBoss AS 7 environment. The example can be run from the Maven
 commandline, or from the Eclipse environment. 
 
-To run the example from the commandline simply type:
+However, before the example can be run successfully please follow the instructions in the 
+"Add a Management or Application User" section of the parent README.md.
+
+By default, the JMS messaging provider is not deployed with the standalone JBoss AS 7 server. You will either need to run 
+a domain server, or configure the standalone server for JMS message. Please refer to the JBoss AS 7 Documentation for further details.
+
+To run the standalone server with JMS enabled:
+
+    standalone.sh -c standalone-full.xml
+
+To run the quickstart from the commandline:
 
     mvn compile exec:java
 
+If the quickstart is successful with the default configuration you will see output similar to this:
+
+    Mar 14, 2012 1:38:58 PM org.jboss.as.quickstarts.jms.HelloWorldJMSClient main
+    INFO: Attempting to acquire connection factory "jms/RemoteConnectionFactory"
+    Mar 14, 2012 1:38:58 PM org.jboss.as.quickstarts.jms.HelloWorldJMSClient main
+    INFO: Found connection factory "jms/RemoteConnectionFactory" in JNDI
+    Mar 14, 2012 1:38:58 PM org.jboss.as.quickstarts.jms.HelloWorldJMSClient main
+    INFO: Attempting to acquire destination "jms/queue/test"
+    Mar 14, 2012 1:38:58 PM org.jboss.as.quickstarts.jms.HelloWorldJMSClient main
+    INFO: Found destination "jms/queue/test" in JNDI
+    Mar 14, 2012 1:38:58 PM org.jboss.as.quickstarts.jms.HelloWorldJMSClient main
+    INFO: Sending 1 messages with content: Hello, World!
+    Mar 14, 2012 1:38:58 PM org.jboss.as.quickstarts.jms.HelloWorldJMSClient main
+    INFO: Received message with content Hello, World!
+
 The example provides for a certain amount of customization for the `mvn:exec` plugin via system properties explained below:
-
-
-Required Properties
--------------------
-
-* `connection.type`
-   
-   Which connection type to the JMS provider you want to use. Accepted values are `netty` or `jndi`.
-   
-   Default: `netty`
-   
-   *Note:* Currently external JNDI access is under development in JBoss AS 7. As a result netty is the default value.
-		
-* `java.naming.factory.initial`
-
-   The JNDI initial context factory you wish to use
-   
-   Default: `org.jboss.as.naming.InitialContextFactory`
-
-* `java.naming.provider.url`
-   
-   The provider URL of the JNDI context you wish to use
-
-   Default: `jnp://localhost:1099`
 
 
 Optional Properties
 -------------------
 
-* `cf.name`
-
-   The JNDI name of the JMS ConnectionFactory you want to use.
-
-   Default: `RemoteConnectionFactory`
-
-* `dest.name`
-
-   The JNDI name of the JMS Destination you want to use.
+* `username`
    
-   Default: `testQueue`
+    This username is used for both the JMS connection and the JNDI look-up.  See the instructions in the 
+    "Add a Management or Application User" section of the parent README.md for more information.
+   
+    Default: `quickstartUser`
+		
+* `password`
 
-* `message.content`
+    This password is used for both the JMS connection and the JNDI look-up.  See the instructions in the 
+    "Add a Management or Application User" section of the parent README.md for more information.
+   
+    Default: `quickstartPassword`
 
-   The content of the JMS TextMessage.
-	
-   Default: `"Hello, World!"`
+* `connection.factory`
+
+    The name of the JMS ConnectionFactory you want to use.
+
+    Default: `jms/RemoteConnectionFactory`
+
+* `destination`
+
+    The name of the JMS Destination you want to use.
+   
+    Default: `jms/queue/test`
 
 * `message.count`
 
-   The number of JMS messages you want to produce and consume.
+    The number of JMS messages you want to produce and consume.
 
-   Default: `1`
+    Default: `1`
+
+* `message.content`
+
+    The content of the JMS TextMessage.
 	
+    Default: `"Hello, World!"`
 
-Deployment Note
----------------
-
-By default, the JMS messaging provider is not deployed with the standalone JBoss AS 7 server. You will either need to run 
-a domain server, or configure the standalone server for JMS message. Please refer to the JBoss AS 7 Documentation for further details.
-
-To run the standalone server with JMS Messaging enabled
-
-    standalone.sh -c standalone-full.xml
 
 
 	
--- helloworld-jms/src/main/java/org/jboss/as/quickstarts/jms/HelloWorldJMSClient.java
+++ helloworld-jms/src/main/java/org/jboss/as/quickstarts/jms/HelloWorldJMSClient.java
@@ -1,6 +1,7 @@
 package org.jboss.as.quickstarts.jms;
 
 import java.util.logging.Logger;
+import java.util.Properties;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
@@ -9,14 +10,23 @@ import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
 
 public class HelloWorldJMSClient {
     private static final Logger log = Logger.getLogger(HelloWorldJMSClient.class.getName());
 
+    // Set up all the deafult values
     private static final String DEFAULT_MESSAGE = "Hello, World!";
-    private static final int DEFAULT_MESSAGE_COUNT = 1;
+    private static final String DEFAULT_CONNECTION_FACTORY = "jms/RemoteConnectionFactory";
+    private static final String DEFAULT_DESTINATION = "jms/queue/test";
+    private static final String DEFAULT_MESSAGE_COUNT = "1";
+    private static final String DEFAULT_USERNAME = "quickstartUser";
+    private static final String DEFAULT_PASSWORD = "quickstartPassword";
+    private static final String INITIAL_CONTEXT_FACTORY = "org.jboss.naming.remote.client.InitialContextFactory";
+    private static final String PROVIDER_URL = "remote://localhost:4447";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         ConnectionFactory connectionFactory = null;
         Connection connection = null;
@@ -25,40 +35,60 @@ public class HelloWorldJMSClient {
         MessageConsumer consumer = null;
         Destination destination = null;
         TextMessage message = null;
+        Context context = null;
 
         try {
-            connectionFactory = JMSClientUtil.getConnectionFactory();
-            destination = JMSClientUtil.getDestination();
-            connection = connectionFactory.createConnection();
+            // Set up the context for the JNDI lookup
+            final Properties env = new Properties();
+            env.put(Context.INITIAL_CONTEXT_FACTORY, INITIAL_CONTEXT_FACTORY);
+            env.put(Context.PROVIDER_URL, PROVIDER_URL);
+            env.put(Context.SECURITY_PRINCIPAL, System.getProperty("username", DEFAULT_USERNAME));
+            env.put(Context.SECURITY_CREDENTIALS, System.getProperty("password", DEFAULT_PASSWORD));
+            context = new InitialContext(env);
+
+            // Perform the JNDI lookups
+            String connectionFactoryString = System.getProperty("connection.factory", DEFAULT_CONNECTION_FACTORY);
+            log.info("Attempting to acquire connection factory \"" + connectionFactoryString + "\"");
+            connectionFactory = (ConnectionFactory) context.lookup(connectionFactoryString);
+            log.info("Found connection factory \"" + connectionFactoryString + "\" in JNDI");            
+
+            String destinationString = System.getProperty("destination", DEFAULT_DESTINATION);
+            log.info("Attempting to acquire destination \"" + destinationString + "\"");
+            destination = (Destination) context.lookup(destinationString);
+            log.info("Found destination \"" + destinationString + "\" in JNDI");
+
+            // Create the JMS connection, session, producer, and consumer
+            connection = connectionFactory.createConnection(System.getProperty("username", DEFAULT_USERNAME), System.getProperty("password", DEFAULT_PASSWORD));
             session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
             producer = session.createProducer(destination);
             consumer = session.createConsumer(destination);
             connection.start();
 
-            int count = (System.getProperty("message.count") == null) ? DEFAULT_MESSAGE_COUNT : Integer.valueOf(System
-                    .getProperty("message.count"));
-            String content = (System.getProperty("message.content") == null) ? DEFAULT_MESSAGE : System
-                    .getProperty("message.content");
+            int count = Integer.parseInt(System.getProperty("message.count", DEFAULT_MESSAGE_COUNT));
+            String content = System.getProperty("message.content", DEFAULT_MESSAGE);
 
             log.info("Sending " + count + " messages with content: " + content);
 
+            // Send the specified number of messages
             for (int i = 0; i < count; i++) {
-                message = session.createTextMessage();
-                message.setText(content);
+                message = session.createTextMessage(content);
                 producer.send(message);
-
             }
 
+            // Then receive the same number of messaes that were sent
             for (int i = 0; i < count; i++) {
-                message = (TextMessage) consumer.receive();
+                message = (TextMessage) consumer.receive(5000);
                 log.info("Received message with content " + message.getText());
             }
-        } catch (Exception e) {
-            log.severe(e.getMessage());
         } finally {
-            JMSClientUtil.closeResources(producer, consumer, session, connection);
+            if (context != null) {
+                context.close();
+            }
+
+            // closing the connection takes care of the session, producer, and consumer
+            if (connection != null) {
+                connection.close();
+            }
         }
-
     }
-
 }
