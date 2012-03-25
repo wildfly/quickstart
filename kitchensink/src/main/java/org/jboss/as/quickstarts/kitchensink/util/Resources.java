d59196dfbac40e1f1a58d7ba15e2eb791685047d
Pete Muir <pmuir@bleepbleep.org.uk>
2012-03-25 13:00:15 +0100

    Whitespace

--- kitchensink/src/main/java/org/jboss/as/quickstarts/kitchensink/util/Resources.java
+++ kitchensink/src/main/java/org/jboss/as/quickstarts/kitchensink/util/Resources.java
@@ -32,10 +32,11 @@ public class Resources {
    public Logger produceLog(InjectionPoint injectionPoint) {
       return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
    }
-
+   
    @Produces
    @RequestScoped
    public FacesContext produceFacesContext() {
       return FacesContext.getCurrentInstance();
    }
+   
 }
