jboss-developer/jboss-eap-quickstarts

Added Arquillian tests to ejbinwar

ejb-in-war/src/test/java/org/jboss/as/quickstarts/ejbinwar/ejb/GreeterEJBTest.java
@@ -1,0 +1,57 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc. and/or its affiliates,
+ * and individual contributors as indicated by the @author tags.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ *
+ * (C) 2011,
+ * @author JBoss, by Red Hat.
+ */
+package org.jboss.as.quickstarts.ejbinwar.ejb;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.ejb.EJB;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.quickstarts.ejbinwar.ejb.GreeterEJB;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * A simple test case using Arquillian to test an EJB.
+ *
+ * @author david@davidsalter.co.uk
+ */
+@RunWith(Arquillian.class)
+public class GreeterEJBTest {
+	
+	@Deployment
+	public static JavaArchive createDeployment() {
+		return ShrinkWrap.create(JavaArchive.class, "test.jar")
+			.addClasses(GreeterEJB.class);
+	}
+
+	@EJB
+	private GreeterEJB greeterEJB;
+
+	@Test
+	public void testGreeter() throws Exception {
+		String name = "World!";
+		assertEquals("Hello " + name, greeterEJB.sayHello(name));
+	}
+}

ejb-in-war/src/test/java/org/jboss/as/quickstarts/ejbinwar/controller/GreeterTest.java
@@ -1,0 +1,63 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc. and/or its affiliates,
+ * and individual contributors as indicated by the @author tags.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ *
+ * (C) 2011,
+ * @author JBoss, by Red Hat.
+ */
+package org.jboss.as.quickstarts.ejbinwar.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.quickstarts.ejbinwar.controller.Greeter;
+import org.jboss.as.quickstarts.ejbinwar.ejb.GreeterEJB;
+import org.jboss.shrinkwrap.api.ArchivePaths;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * A simple test case using Arquillian to test a CDI bean.
+ *
+ * @author david@davidsalter.co.uk
+ */
+@RunWith(Arquillian.class)
+public class GreeterTest {
+	
+	@Deployment
+	public static JavaArchive createTestArchive() {
+		return ShrinkWrap.create(JavaArchive.class, "test.jar")
+		.addClasses(Greeter.class, GreeterEJB.class)
+		.addAsManifestResource(EmptyAsset.INSTANCE, ArchivePaths.create("beans.xml"));
+	}
+
+	@Inject
+	Greeter greeter;
+
+	@Test
+	public void testGetMessage() throws Exception {
+		String name="World!";
+		greeter.setName(name);
+
+		assertEquals("Hello " + name, greeter.getMessage());
+	}
+}

wildfly/quickstart

ejb-in-war/src/test/java/org/jboss/as/quickstarts/ejbinwar/test/GreeterEJBTest.java
@@ -1,0 +1,57 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc. and/or its affiliates,
+ * and individual contributors as indicated by the @author tags.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ *
+ * (C) 2011,
+ * @author JBoss, by Red Hat.
+ */
+package org.jboss.as.quickstarts.ejbinwar.test;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.ejb.EJB;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.quickstarts.ejbinwar.ejb.GreeterEJB;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * A simple test case using Arquillian to test an EJB.
+ *
+ * @author david@davidsalter.co.uk
+ */
+@RunWith(Arquillian.class)
+public class GreeterEJBTest {
+	
+	@Deployment
+	public static JavaArchive createDeployment() {
+		return ShrinkWrap.create(JavaArchive.class, "test.jar")
+			.addClasses(GreeterEJB.class);
+	}
+
+	@EJB
+	private GreeterEJB greeterEJB;
+
+	@Test
+	public void testGreeter() throws Exception {
+		String name = "World!";
+		assertEquals("Hello " + name, greeterEJB.sayHello(name));
+	}
+}

ejb-in-war/src/test/java/org/jboss/as/quickstarts/ejbinwar/test/GreeterTest.java
@@ -1,0 +1,63 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc. and/or its affiliates,
+ * and individual contributors as indicated by the @author tags.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ *
+ * (C) 2011,
+ * @author JBoss, by Red Hat.
+ */
+package org.jboss.as.quickstarts.ejbinwar.test;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.quickstarts.ejbinwar.controller.Greeter;
+import org.jboss.as.quickstarts.ejbinwar.ejb.GreeterEJB;
+import org.jboss.shrinkwrap.api.ArchivePaths;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * A simple test case using Arquillian to test a CDI bean.
+ *
+ * @author david@davidsalter.co.uk
+ */
+@RunWith(Arquillian.class)
+public class GreeterTest {
+	
+	@Deployment
+	public static JavaArchive createTestArchive() {
+		return ShrinkWrap.create(JavaArchive.class, "test.jar")
+		.addClasses(Greeter.class, GreeterEJB.class)
+		.addAsManifestResource(EmptyAsset.INSTANCE, ArchivePaths.create("beans.xml"));
+	}
+
+	@Inject
+	Greeter greeter;
+
+	@Test
+	public void testGetMessage() throws Exception {
+		String name="World!";
+		greeter.setName(name);
+
+		assertEquals("Hello " + name, greeter.getMessage());
+	}
+}