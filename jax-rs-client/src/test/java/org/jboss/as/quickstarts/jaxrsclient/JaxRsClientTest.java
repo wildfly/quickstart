cc70b2f2ecdcb4cb88da15d4d881e8ca2dd25cd3
Blaine Mincey <blaine.mincey@gmail.com>
2012-04-11 11:40:49 -0400

    Add OpenShift support to jax-rs-client
    * Added surefire plugin and system props to pom.xml.
    * Read from system props in JaxRsClientTest.java.
    * Added comment to the beforeClass method.
    * Fix whitespace

--- jax-rs-client/src/test/java/org/jboss/as/quickstarts/jaxrsclient/JaxRsClientTest.java
+++ jax-rs-client/src/test/java/org/jboss/as/quickstarts/jaxrsclient/JaxRsClientTest.java
@@ -1,4 +1,5 @@
 package org.jboss.as.quickstarts.jaxrsclient;
+
 /**
  * This example demonstrates the use an external JAX-RS RestEasy client
  * which interacts with a JAX-RS Web service that uses CDI 1.0 and JAX-RS 
@@ -18,58 +19,70 @@ import javax.ws.rs.core.MediaType;
 import org.apache.http.client.ClientProtocolException;
 import org.jboss.resteasy.client.ClientRequest;
 import org.jboss.resteasy.client.ClientResponse;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 /**
- * JUnit4 Test class which makes a request to the RESTful
- * helloworld-rs web service.
+ * JUnit4 Test class which makes a request to the RESTful helloworld-rs web service.
  * 
- * @author bmincey
- *
+ * @author bmincey (Blaine Mincey)
+ * 
  */
-public class JaxRsClientTest
-{
-    /** 
-     * Request URLs of the RESTful web service
+public class JaxRsClientTest {
+    /**
+     * Request URLs pulled from system properties in pom.xml
      */
-    private static final String XML_URL = "http://localhost:8080/jboss-as-helloworld-rs/xml";
-    private static final String JSON_URL = "http://localhost:8080/jboss-as-helloworld-rs/json";
-    
+    private static String XML_URL;
+    private static String JSON_URL;
+
+    /**
+     * Property names used to pull values from system properties in pom.xml
+     */
+    private static final String XML_PROPERTY = "xmlUrl";
+    private static final String JSON_PROPERTY = "jsonUrl";
+
     /**
      * Responses of the RESTful web service
      */
     private static final String XML_RESPONSE = "<xml><result>Hello World!</result></xml>";
     private static final String JSON_RESPONSE = "{\"result\":\"Hello World!\"}";
-    
-    
+
     /**
-     * Test method which executes the runRequest method that calls the RESTful 
-     * helloworld-rs web service.
+     * Method executes BEFORE the test method. Values are read from system properties that can be modified in the pom.xml.
+     */
+    @BeforeClass
+    public static void beforeClass() {
+        JaxRsClientTest.XML_URL = System.getProperty(JaxRsClientTest.XML_PROPERTY);
+        JaxRsClientTest.JSON_URL = System.getProperty(JaxRsClientTest.JSON_PROPERTY);
+    }
+
+    /**
+     * Test method which executes the runRequest method that calls the RESTful helloworld-rs web service.
      */
     @Test
-    public void test()
-    {
-        assertEquals("XML Response", JaxRsClientTest.XML_RESPONSE, this.runRequest(JaxRsClientTest.XML_URL, MediaType.APPLICATION_XML_TYPE));
-        assertEquals("JSON Response", JaxRsClientTest.JSON_RESPONSE, this.runRequest(JaxRsClientTest.JSON_URL, MediaType.APPLICATION_JSON_TYPE));
+    public void test() {
+        assertEquals("XML Response", JaxRsClientTest.XML_RESPONSE,
+                this.runRequest(JaxRsClientTest.XML_URL, MediaType.APPLICATION_XML_TYPE));
+
+        assertEquals("JSON Response", JaxRsClientTest.JSON_RESPONSE,
+                this.runRequest(JaxRsClientTest.JSON_URL, MediaType.APPLICATION_JSON_TYPE));
     }
-    
+
     /**
      * The purpose of this method is to run the external REST request.
+     * 
      * @param url The url of the RESTful service
      * @param mediaType The mediatype of the RESTful service
      */
-    private String runRequest(String url, MediaType mediaType)
-    {
+    private String runRequest(String url, MediaType mediaType) {
         String result = null;
-        
+
         System.out.println("===============================================");
         System.out.println("URL: " + url);
         System.out.println("MediaType: " + mediaType.toString());
-        
-        try
-        {
-            // Using the RESTEasy libraries, initiate a client request 
+
+        try {
+            // Using the RESTEasy libraries, initiate a client request
             // using the url as a parameter
             ClientRequest request = new ClientRequest(url);
 
@@ -81,40 +94,31 @@ public class JaxRsClientTest
 
             // Check the HTTP status of the request
             // HTTP 200 indicates the request is OK
-            if (response.getStatus() != 200)
-            {
-                throw new RuntimeException("Failed request with HTTP status: "
-                        + response.getStatus());
+            if (response.getStatus() != 200) {
+                throw new RuntimeException("Failed request with HTTP status: " + response.getStatus());
             }
 
             // We have a good response, let's now read it
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    new ByteArrayInputStream(response.getEntity().getBytes())));
+            BufferedReader br = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(response.getEntity()
+                    .getBytes())));
 
             // Loop over the br in order to print out the contents
             System.out.println("\n*** Response from Server ***\n");
             String output = null;
-            while ((output = br.readLine()) != null)
-            {
+            while ((output = br.readLine()) != null) {
                 System.out.println(output);
                 result = output;
             }
-        }
-        catch (ClientProtocolException cpe)
-        {
+        } catch (ClientProtocolException cpe) {
             System.err.println(cpe);
-        }
-        catch (IOException ioe)
-        {
+        } catch (IOException ioe) {
             System.err.println(ioe);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.err.println(e);
         }
-        
+
         System.out.println("\n===============================================");
-        
+
         return result;
     }
 
