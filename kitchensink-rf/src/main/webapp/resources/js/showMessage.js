2feccfb7507d5eeefaf6eaccaf3b9b52de67c4a0
Brian Leathem <bleathem@gmail.com>
2012-05-04 13:23:21 -0700

    RF-12227: changed the showmessage definition

--- kitchensink-rf/src/main/webapp/resources/js/showMessage.js
+++ kitchensink-rf/src/main/webapp/resources/js/showMessage.js
@@ -1,4 +1,4 @@
-function showMessage(message) {
+showMessage = function (message) {
     jQuery('<div />', {class: 'rf-p-hdr'}).prependTo('#newMemberMessages').text(message).fadeOut(3000, function() {
       $(this).remove();
     });
