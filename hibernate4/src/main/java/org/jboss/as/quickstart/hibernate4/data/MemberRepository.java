09398822703dab25be8141a0ce21ed100e1bcfa9
Madhumita Sadhukhan <madhumita.sadhukhan@gmail.com>
2012-05-06 17:04:16 +0200

    added/modified files to implement UI changes in sync with kitchensink

--- hibernate4/src/main/java/org/jboss/as/quickstart/hibernate4/data/MemberRepository.java
+++ hibernate4/src/main/java/org/jboss/as/quickstart/hibernate4/data/MemberRepository.java
@@ -9,6 +9,7 @@ import javax.persistence.criteria.Root;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
 
@@ -23,17 +24,6 @@ public class MemberRepository {
    public Member findById(Long id) {
       return em.find(Member.class, id);
    }
-
-   /*public Member findByEmail(String email) {
-      CriteriaBuilder cb = em.getCriteriaBuilder();
-      CriteriaQuery<Member> criteria = cb.createQuery(Member.class);
-      Root<Member> member = criteria.from(Member.class);
-      // Swap criteria statements if you would like to try out type-safe criteria queries, a new
-      // feature in JPA 2.0
-      // criteria.select(member).where(cb.equal(member.get(Member_.name), email));
-      criteria.select(member).where(cb.equal(member.get("email"), email));
-      return em.createQuery(criteria).getSingleResult();
-   }*/
 
    public List<Member> findAllOrderedByName() {
 
