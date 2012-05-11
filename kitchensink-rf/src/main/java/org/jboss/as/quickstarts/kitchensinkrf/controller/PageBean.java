03575d1e3dfe547a5445c91606847facdb057bf3
Lukas Fryc <lfryc@redhat.com>
2012-05-11 14:46:24 +0200

    fixed PageBean: wrong package

--- kitchensink-rf/src/main/java/org/jboss/as/quickstarts/kitchensinkrf/controller/PageBean.java
+++ kitchensink-rf/src/main/java/org/jboss/as/quickstarts/kitchensinkrf/controller/PageBean.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  **/
-package org.jboss.as.quickstarts.kitchensinkrfrf.controller;
+package org.jboss.as.quickstarts.kitchensinkrf.controller;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
