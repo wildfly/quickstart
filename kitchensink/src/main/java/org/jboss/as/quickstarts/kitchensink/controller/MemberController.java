f8c4430025008e7f1a2969a10782104f9a9d6fc6
Sande Gilda <sgilda@redhat.com>
2012-05-14 12:01:18 -0400

    Fix error handling for duplicate key and other exceptions: display root cause

--- kitchensink/src/main/java/org/jboss/as/quickstarts/kitchensink/controller/MemberController.java
+++ kitchensink/src/main/java/org/jboss/as/quickstarts/kitchensink/controller/MemberController.java
@@ -49,13 +49,39 @@ public class MemberController {
    }
 
    public void register() throws Exception {
-      memberRegistration.register(newMember);
-      facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Registered!", "Registration successful"));
-      initNewMember();
+      try {
+          memberRegistration.register(newMember);
+          facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Registered!", "Registration successful"));
+          initNewMember();
+      }
+	  catch(Exception e) {
+	      String errorMessage = getRootErrorMessage(e);
+	      facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, "Registration unsuccessful"));
+	  }
    }
 
    @PostConstruct
    public void initNewMember() {
       newMember = new Member();
    }
+
+   private String getRootErrorMessage(Exception e) {
+       // Default to general error message that registration failed.
+       String errorMessage = "Registration failed. See server log for more information";
+       if (e == null) {
+      	   // This shouldn't happen, but return the default messages
+      	   return errorMessage;
+       }
+       
+       // Start with the exception and recurse to find the root cause
+       Throwable t = e;
+       while (t != null) {
+      	   // Get the message from the Throwable class instance
+      	   errorMessage = t.getLocalizedMessage();
+      	   t = t.getCause();   	   
+       }
+       // This is the root cause message
+	   return errorMessage;
+   }
+
 }
