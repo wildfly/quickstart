b98c293e765de8e129c9978679901c11a4084324
James Strachan <james.strachan@gmail.com>
2013-02-01 14:01:51 +0000

    render server side generated errors in the form

--- kitchensink-angularjs/src/main/webapp/js/controllers.js
+++ kitchensink-angularjs/src/main/webapp/js/controllers.js
@@ -1,49 +1,50 @@
 function MembersCtrl($scope, $http) {
 
-    
-	var defaultNewMember = {
-	    name: "",
-	    email: "",
-	    phoneNumber: ""
-	};
-	
-	updateList = function() {
-		$http.get('rest/members').success(function(data) { 
-			$scope.members = data;
-		});
-	};
-	
-	updateList();
-	
-	$scope.orderProp = 'name';
-	
-	$scope.newMember = defaultNewMember;
-	
-	$scope.reset = function() {
-		//clear input fields
-        $scope.newMember = {};
-	};
-	
-	$scope.register = function() {
-		$scope.successMessages = '';
-		$scope.errorMessages = '';
-		$http.post('rest/members', $scope.newMember).success(function(data) {
-            
-            //mark success on the registration form
-            $scope.successMessages = [ 'Member Registered' ];
-            
-            // Update the list of members
-            updateList();
-            
-            //Clear the form
-            $scope.reset();
-		}).error(function(data, status) {
-			if ((status == 409) || (status == 400)) {
-                // TODO bind errors messages
-            } else {
+  var defaultNewMember = {
+    name: "",
+    email: "",
+    phoneNumber: ""
+  };
+
+  updateList = function () {
+    $http.get('rest/members').success(function (data) {
+      $scope.members = data;
+    });
+  };
+
+  updateList();
+
+  $scope.orderProp = 'name';
+
+  $scope.newMember = defaultNewMember;
+
+  $scope.reset = function () {
+    //clear input fields
+    $scope.newMember = {};
+  };
+
+  $scope.register = function () {
+    $scope.successMessages = '';
+    $scope.errorMessages = '';
+    $http.post('rest/members', $scope.newMember).success(function (data) {
+      //mark success on the registration form
+      $scope.successMessages = [ 'Member Registered' ];
+
+      // Update the list of members
+      updateList();
+
+      //Clear the form
+      $scope.errors = {};
+      $scope.reset();
+      $scope.$apply();
+    }).error(function (data, status) {
+              if ((status == 409) || (status == 400)) {
+                $scope.errors = data;
+              } else {
                 //console.log("error - unknown server issue");
-            	$scope.errorMessages = ['Unknown  server error'];
-            }
-		});
-	};
+                $scope.errorMessages = ['Unknown  server error'];
+              }
+              $scope.$apply();
+            });
+  };
 }
