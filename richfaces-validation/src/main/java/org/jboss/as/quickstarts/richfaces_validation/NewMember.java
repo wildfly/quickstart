9e483876dfaf8a802800e9ffd4e217cdb77ab63f
Sande Gilda <sgilda@redhat.com>
2013-03-22 12:46:20 -0400

    Modify Java license comment style so it is not a JavaDoc comment

--- richfaces-validation/src/main/java/org/jboss/as/quickstarts/richfaces_validation/NewMember.java
+++ richfaces-validation/src/main/java/org/jboss/as/quickstarts/richfaces_validation/NewMember.java
@@ -1,4 +1,4 @@
-/**
+/*
  * JBoss, Home of Professional Open Source
  * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
  * contributors by the @authors tag. See the copyright.txt in the
