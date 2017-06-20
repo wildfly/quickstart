65061d2d31dd54a9110d0e46a8e924e6ba33b0c9
Tomaz Cerar <tomaz.cerar@gmail.com>
2017-06-20 18:16:11 +0200

    fix build

--- security-vault-askpass/bin/askpass.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# "Enter passphrase for ..." is a prefix required by kwallet for example
-if [ "$1" = "Enter passphrase for askpass quickstart" ]; then
-    echo vault22
-else
-    echo $1
-fi
