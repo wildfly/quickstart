d4b6f52e112eb00deb180c336d336af78cbf4259
emmartins <emartins@redhat.com>
2018-10-23 06:00:42 +0100

    WFLY-11210: replaces EJB annotations with JNDI lookups

--- inter-app/appB/src/main/java/org/jboss/as/quickstarts/interapp/appB/Imports.java
+++ inter-app/appB/src/main/java/org/jboss/as/quickstarts/interapp/appB/Imports.java
@@ -16,13 +16,14 @@
  */
 package org.jboss.as.quickstarts.interapp.appB;
 
-import javax.ejb.EJB;
 import javax.enterprise.inject.Produces;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 import org.jboss.as.quickstarts.interapp.shared.Foo;
 
 /**
- * The Imports class is used to alias EJBs imported from other applications as local CDI beans, thus allowing consumers to
+ * The Imports class is used to alias EJBs looked up from other applications as local CDI beans, thus allowing consumers to
  * ignore the details of inter-application communication.
  *
  * @author Pete Muir
@@ -31,8 +32,9 @@ import org.jboss.as.quickstarts.interapp.shared.Foo;
 public class Imports {
 
     @Produces
-    @EJB(lookup = "java:global/inter-app-appA/FooImpl!org.jboss.as.quickstarts.interapp.shared.Foo")
-    private Foo foo;
+    private Foo lookupFoo() throws NamingException {
+        return InitialContext.doLookup("java:global/inter-app-appA/FooImpl!org.jboss.as.quickstarts.interapp.shared.Foo");
+    }
 
     private Imports() {
         // Disable instantiation of this class
