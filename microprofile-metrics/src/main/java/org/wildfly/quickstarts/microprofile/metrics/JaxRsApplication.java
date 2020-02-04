4631506ba55f20dbe947b5349785d1137af46768
xstefank <xstefank122@gmail.com>
2020-02-04 09:42:53 +0100

    [WFLY-12809] Quickstart for MP Metrics 2.3

---
+++ microprofile-metrics/src/main/java/org/wildfly/quickstarts/microprofile/metrics/JaxRsApplication.java
@@ -0,0 +1,8 @@
+package org.wildfly.quickstarts.microprofile.metrics;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class JaxRsApplication extends Application {
+}
