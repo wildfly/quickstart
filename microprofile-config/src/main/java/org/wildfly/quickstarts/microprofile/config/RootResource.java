3918784b86d9b33c73a29156e0e7440a56bcc1c8
Jeff Mesnil <jmesnil@redhat.com>
2021-02-05 15:23:31 +0100

    microprofile-config: add homepage
    Display a list of the available endpoints when the application is
    opened.

--- microprofile-config/src/main/java/org/wildfly/quickstarts/microprofile/config/RootResource.java
+++ microprofile-config/src/main/java/org/wildfly/quickstarts/microprofile/config/RootResource.java
@@ -9,8 +9,23 @@ import javax.ws.rs.core.MediaType;
 public class RootResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getRootResponse() {
-        return "MicroProfile Config quickstart deployed successfully. You can find the available operations in the included README file.";
+    @Produces(MediaType.TEXT_HTML)
+    public String getHomePage() {
+        return "<html>\n" +
+                "<body>\n" +
+                "<h1>MicroProfile Config Quickstart</h1>\n" +
+                "\n" +
+                "<h2>List of endpoints</h2>\n" +
+                "<ul>\n" +
+                "    <li><a href=\"/config/value\">/config/value</a></li>\n" +
+                "    <li><a href=\"/config/required\">/config/required</a></li>\n" +
+                "    <li><a href=\"/config/optional\">/config/optional</a></li>\n" +
+                "    <li><a href=\"/config/all-props\">/config/all-props</a></li>\n" +
+                "    <li><a href=\"/converter/value\">/converter/value</a></li>\n" +
+                "    <li><a href=\"/custom-config/value\">/custom-config/value</a></li>\n" +
+                "    <li><a href=\"/custom-config/reloaded-value\">/custom-config/reloaded-value</a></li>\n" +
+                "</ul>\n" +
+                "</body>\n" +
+                "</html>\n";
     }
 }
