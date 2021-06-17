e8cdb82b060a62acb1261bc61e3468f100d72090
Eduardo Martins <emartins@redhat.com>
2021-06-17 11:47:28 +0100

    [WFLY-14827] remove unused imports

--- tasks-jsf/src/main/java/org/jboss/as/quickstarts/tasksJsf/Resources.java
+++ tasks-jsf/src/main/java/org/jboss/as/quickstarts/tasksJsf/Resources.java
@@ -22,7 +22,6 @@ import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
-import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
