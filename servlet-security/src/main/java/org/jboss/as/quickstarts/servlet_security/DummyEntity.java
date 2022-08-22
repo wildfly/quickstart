8b68da664cafd93456b2e53380ba46d33aef32a7
Farah Juma <fjuma@redhat.com>
2022-08-22 18:14:30 -0400

    [WFLY-16195] Add a dummy entity for servlet-security in order for the import.sql script to get run

---
+++ servlet-security/src/main/java/org/jboss/as/quickstarts/servlet_security/DummyEntity.java
@@ -0,0 +1,33 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2022, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstarts.servlet_security;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@SuppressWarnings("serial")
+@Entity
+// This class should be removed once https://issues.redhat.com/browse/WFLY-16871 is resolved
+public class DummyEntity implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+}
