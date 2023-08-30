36f822d8f0075b46dba9b966bde3d5695c00d5e9
Eduardo Martins <emartins@redhat.com>
2023-08-30 14:21:34 +0100

    [WFLY-18479][WFLY-18493][WFLY-18523] the starting point for the CY2023Q3 QS enhancements, for now targeting only helloworld and microprofile-config The following enhancements are included: * maven project version properties rework * maven project plugin management * server provisioning for non MP QS * unified openshift build for all QS, including MP ones, using the standard server provisioning (no bootable jar) * common functionality to provide basic runtime testing for all QS, including MP ones (integration-testing maven profile, Abstract + BasicRuntimeIT, github actions CI) * project CI as specified by WFLY-18523 * reworked asciidoc sections wrt build, run and test for bare metal, server provisioning/bootable-jar, openshift

---
+++ helloworld/src/test/java/org/wildfly/quickstarts/helloworld/BasicRuntimeIT.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2022 JBoss by Red Hat.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.quickstarts.helloworld;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * The very basic runtime integration testing.
+ * @author Emmanuel Hugonnet
+ * @author emartins
+ */
+public class BasicRuntimeIT {
+
+    private static final String DEFAULT_SERVER_HOST = "http://localhost:8080/helloworld";
+
+    @Test
+    public void testHTTPEndpointIsAvailable() throws IOException, InterruptedException, URISyntaxException {
+        String serverHost = System.getenv("SERVER_HOST");
+        if (serverHost == null) {
+            serverHost = System.getProperty("server.host");
+        }
+        if (serverHost == null) {
+            serverHost = DEFAULT_SERVER_HOST;
+        }
+        final HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(serverHost+"/HelloWorld"))
+                .GET()
+                .build();
+        final HttpClient client = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.ALWAYS)
+                .connectTimeout(Duration.ofMinutes(1))
+                .build();
+        final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, response.statusCode());
+    }
+}
