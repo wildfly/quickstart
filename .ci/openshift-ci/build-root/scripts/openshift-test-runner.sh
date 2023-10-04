74b38d784459bc752e4e926fd5192aa025d2d25d
Kabir Khan <kkhan@redhat.com>
2023-10-04 17:50:55 +0100

    Initial scripts for QS testing on OpenShift CI

---
+++ .ci/openshift-ci/build-root/scripts/openshift-test-runner.sh
@@ -0,0 +1,236 @@
+#!/bin/sh
+# The main script to run the quickstarts
+# It iterates over all found quickstart directories, ignoring the ones found in
+# excluded-directories.txt, and runs the run-quickstart-test-on-openshift-for each of them
+#
+# To debug the test script if something goes wrong with a pull request, add a file to the quickstarts root
+# called 'enable-wait', which will cause everything to pause for an hour before running any tests.
+
+RED_CONSOLE="\033[0;31m"
+GREEN_CONSOLE="\033[0;32m"
+NOCOLOUR_CONSOLE="\033[0m"
+
+
+
+poll_marker_files() {
+  seconds=$1
+  now=$(date +%s)
+  end=$(($seconds + $now))
+
+  found_file=""
+  while [ $now -lt $end ]; do
+    sleep 1
+    now=$(date +%s)
+    if [ -f "continue" ]; then
+      found_file="continue"
+      break
+    elif [ -f "exit" ]; then
+      found_file="exit"
+      break
+    fi
+  done
+  echo ${found_file}
+}
+
+wait_marker_files() {
+  echo "Waiting $seconds. 'oc rsh' in and either 'touch continue' to stop waiting, or 'touch exit' to abort the test run. The latter will result in the test being reported as failed"
+  found_file=$(poll_marker_files $1)
+  if [ -z "${found_file}" ]; then
+      echo "Wait timed out - continuing"
+  elif [ "${found_file}" = "exit" ]; then
+      echo "'exit' marker file found, exiting the test run"
+      exit 1
+  elif [ "${found_file}" = "continue" ]; then
+      echo "'continue' marker file found, continuing the test run"
+  fi
+}
+
+
+# Runs a single quickstart
+#
+# Parameters
+# 1 - the directory containing this script
+# 2 - the name of the quickstart directory
+runQuickstart() {
+  script_dir="${1}"
+  qs_dir="${2}"
+
+  echo "================================================================="
+  echo "Running tests for ${qs_dir} "
+  echo "================================================================="
+
+
+  "${script_dir}"/run-quickstart-test-on-openshift.sh "${qs_dir}"
+  if [ "$?" = "0" ]; then
+    echo "${GREEN_CONSOLE}Tests for ${qs_dir} PASSED!${NOCOLOUR_CONSOLE}"
+  else
+    echo "${RED_CONSOLE}Tests for ${qs_dir} FAILED!${NOCOLOUR_CONSOLE}"
+    if [ -z "${failed_tests}" ]; then
+      failed_tests="${fileName}"
+    else
+      failed_tests="${failed_tests}, ${fileName}"
+    fi
+  fi
+  echo "---------------------------------------------------------------"
+  echo
+  echo
+}
+
+declare -a test_directories
+
+# Initialises the test_directories variable if this is for a scheduled job
+getAllDirectories() {
+  for file in ${basedir}/*; do
+    fileName=$(basename "${file}")
+    if [ ! -d "${file}" ]; then
+      # echo "${fileName} is not a directory!"
+      continue
+    fi
+    test_directories+=(${fileName})
+  done
+}
+
+
+# Initialises the test_directories variable if this is for a pull request
+getPrTouchedDirs() {
+  echo "Pull request detected. Determining which directories to run tests for..."
+  git remote show upstream &> /dev/null
+  if [ "$?" != "0" ]; then
+    # On CI this will always happen. This is just here for local testing
+    git remote add upstream "${OPENSHIFT_BUILD_SOURCE}"
+  fi
+  git fetch upstream "${PULL_BASE_REF}" &> /dev/null
+  changed_files=$(git diff --name-only  upstream/"${PULL_BASE_REF}"..HEAD)
+
+  declare -a changed_directories
+
+  root_dir_file_changed=0
+  for file in ${changed_files}; do
+    # DON'T quote the expression, or it doesn't filter!
+    if [[ "${file}" == *.adoc ]] || [[ "${file}" == .ci/* ]] || [[ "${file}" == .github/* ]]; then
+      continue
+    fi
+
+    IFS='/' read -ra parts <<< "${file}"
+    if [ "${#parts[@]}" == 1 ]; then
+      echo "Changed detected in ${file} which is in the root directory. All tests will need to be run."
+      root_dir_file_changed=1
+      break
+    else
+      changed_directories+=(${parts[0]})
+    fi
+  done
+
+
+  if [ "${root_dir_file_changed}" == "1" ]; then
+    # Function sets the test_directories variable for us
+    getAllDirectories
+  else
+    # Dedupe the found directories
+    output_dirs=$(printf "%s\n" "${changed_directories[@]}" | sort -u)
+    declare -a tmp
+    for dir in ${output_dirs}; do
+      tmp+=(${dir})
+    done
+    test_directories=(${tmp[@]})
+  fi
+}
+
+# Repopulates the test_directories variable with valid directories
+filterDirectories() {
+  declare -a tmp
+  for fileName in "${test_directories[@]}"; do
+      # Quickstarts that have not been migrated yet
+      # TODO once everything has a quickstart_xxx_ci.yml file we can remove the included-directories check
+      grep -q "^${fileName}$" included-directories.txt
+      is_in_included_txt="$?"
+      if [ "${is_in_included_txt}" != "0" ] && [ ! -f "${basedir}/.github/workflows/quickstart_${fileName}_ci.yml" ]; then
+        # echo "Skipping ${fileName}!"
+        continue
+      fi
+
+      tmp+=(${fileName})
+  done
+  test_directories=(${tmp[@]})
+}
+
+start=$SECONDS
+failed_tests=""
+script_directory="${0%/*}"
+script_directory=$(realpath "${script_directory}")
+cd "${script_directory}"
+basedir="${script_directory}/../../../.."
+
+if [ -f "${basedir}/enable-wait" ]; then
+  # If we find the enable-wait marker file, block for an hour for investigation
+  pushd ${basedir}
+  wait_marker_files 3600
+  popd 
+fi
+if [ -f "${basedir}/abort" ]; then
+  # Add ability to abort test run between quickstart runs
+  echo "${basedir}/abort file found. Exiting"
+  exit 1
+fi
+
+if [ "${JOB_TYPE}" = "presubmit" ]; then
+  getPrTouchedDirs
+
+  if [ -z "${PULL_NUMBER}" ]; then
+    # Only relevant if running locally for development
+    echo "Pull request number not specified in PULL_NUMBER"
+    exit 1
+  fi
+  if [ -z "${JOB_SPEC}" ]; then
+    # Again only for local debug
+    api_url="https://api.github.com/repos/wildfly/quickstart/pulls/${PULL_NUMBER}"
+  else
+    # JOB_SPEC will be set on CI
+    api_url="https://api.github.com/repos/$(echo $JOB_SPEC | jq -r .refs.org)/$(echo $JOB_SPEC | jq -r .refs.repo)/pulls/${PULL_NUMBER}"
+  fi
+  # Figure out the original repository and branch of the pull request. OpenShift CI does not give us the information to figure out the
+  # name of the repository from which the PR was opened, if it was cloned to e.g. kabir/wildfly-qs rather than simply kabir/quickstart
+  echo "Getting PR info from ${api_url}"
+  curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "${api_url}" > .pr_info.json
+  export QS_BUILD_URI=$(jq -r .head.repo.clone_url .pr_info.json)
+  export QS_BUILD_REF=$(jq -r .head.ref .pr_info.json)
+else
+# For now just handle everything that is not a pull request to run all jobs
+  #elif [ "${JOB_TYPE}" = "periodic" ] || [ "${JOB_TYPE}" = "postsubmit" ]; then
+  getAllDirectories
+fi
+
+filterDirectories
+
+echo "Parsed test directories, and determined tests should be run for the following directories:"
+printf "\t%s\n" "${test_directories[@]}"
+echo "Running tests..."
+
+for fileName in "${test_directories[@]}"; do
+  if [ "${DRY_RUN}" = "1" ]; then
+    echo "${fileName}"
+  else
+    runQuickstart "${script_directory}" "${fileName}"
+  fi
+done
+
+
+end=$SECONDS
+duration=$((end - start))
+echo "All tests run in $(($duration / 60))m$(($duration % 60))s."
+
+test_status=0
+if [ -z "${failed_tests}" ]; then
+  echo "${GREEN_CONSOLE}All tests passed!${NOCOLOUR_CONSOLE}"
+else
+  echo "${RED_CONSOLE}There were test failures. See the logs for details. The failed tests were: ${failed_tests} ${NOCOLOUR_CONSOLE}"
+  test_status=1
+fi
+
+if [ -f "${basedir}/enable-wait" ]; then
+  # If we find the emable-wait marker file (used above for debugging),
+  # fail the test run so we don't accidentally merge this file
+  exit 1
+fi
+
+exit ${test_status}
---
+++ .ci/openshift-ci/build-root/scripts/overridable-functions.sh
@@ -0,0 +1,84 @@
+# These functions are 'overridable in the individual quickstarts.
+# To do so create a ./qs-overrides/${qs_dir}/overridable-functions.sh and override the
+# functions you need to. ${qs_dir} in this case is the same as the name of the quickstart directory
+# that you want to tweak
+
+# Installs any prerequisites before doing the Helm install.
+# The current directory is the quickstart directory.
+# The default is to use the quickstart directory as the name, but in some cases
+# a quickstart may need to shorten the name of the application in order to control
+# the length of the resources created by OpenShift
+#
+# Parameters
+# 1 - the name of the qs directory (not the full path)
+function applicationName() {
+  echo "${1}"
+}
+
+
+# Installs any prerequisites before doing the Helm install.
+# The current directory is the quickstart directory
+#
+# Parameters
+# 1 - application name
+function installPrerequisites()
+{
+  application="${1}"
+  echo "No prerequisites required for ${application}"
+}
+
+# Cleans any prerequisites after doing the Helm uninstall.
+# The current directory is the quickstart directory
+#
+# Parameters
+# 1 - application name
+function cleanPrerequisites()
+{
+  application="${1}"
+  echo "No prerequisites to clean for ${application}"
+}
+
+# Performs the 'helm install' command.
+# The current directory is the quickstart directory
+# Parameters
+# 1 - application name
+# 2 - set arguments
+#
+# Returns the exit status of the helm install
+#
+# Additionally the following env vars may be used:
+# * helm_install_timeout - the adjusted timeout for the helm install
+#
+function helmInstall() {
+    application="${1}"
+    helm_set_arguments="$2"
+
+    # '--atomic' waits until the pods are ready, and removes everything if something went wrong
+    # `--timeout` sets the timeout for the wait.
+    # https://helm.sh/docs/helm/helm_install/ has more details
+    # Don't quote ${helm_set_arguments} since then it fails when there are none
+    helm install "${application}" wildfly/wildfly -f charts/helm.yaml  --wait --timeout=${helm_install_timeout} ${helm_set_arguments}
+    echo "$?"
+}
+
+# Checks whether optimized mode should be disabled
+# To disable optimized mode for a quickstart, add this method to its
+# overridable-functions.sh and change the body to 'echo "1"'
+function isOptimizedModeDisabled() {
+  echo "0"
+}
+
+# If the Helm variables set by the parent script (e.g. 'build.enabled') need a prefix, return
+# that here. If e.g "wildfly." is returned, the resulting 'build.enabled' becomes 'wildfly.build.enabled'
+function getHelmSetVariablePrefix() {
+    echo ""
+}
+
+# More output when the helm install has gone wrong
+# Parameters
+# 1 - application name
+#
+function helmInstallFailed() {
+    # Noop - the main work is done elsewhere
+    echo ""
+}
