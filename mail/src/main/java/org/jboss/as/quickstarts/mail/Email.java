30bcf0b929bf4db6bd62a6d72922587d91dc0325
Yeray Borges <yborgess@redhat.com>
2023-11-16 11:15:53 +0000

    [WFLY-18491] Mail Quickstart Common Enhancements Jira issue: https://issues.redhat.com/browse/WFLY-18491

--- mail/src/main/java/org/jboss/as/quickstarts/mail/Email.java
+++ mail/src/main/java/org/jboss/as/quickstarts/mail/Email.java
@@ -16,17 +16,23 @@
  */
 package org.jboss.as.quickstarts.mail;
 
-import java.io.Serializable;
-
 import jakarta.annotation.Resource;
 import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
 import jakarta.mail.Address;
+import jakarta.mail.Folder;
 import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
 import jakarta.mail.Session;
+import jakarta.mail.Store;
 import jakarta.mail.Transport;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
+
+import java.io.IOException;
+import java.io.Serializable;
 
 /**
  * <p>
@@ -36,11 +42,13 @@ import jakarta.mail.internet.MimeMessage;
  * It contains address, subject, and content for the <code>email</code> to be sent.
  * </p>
  * <p>
- * The {@link #send()} method provides the business logic to send the email
+ * The {@link #send()} method provides the business logic to send the email.
+ * The {@link #retrievePop3()} method retrieves all the emails sent to a specific email account using POP3.
+ * The {@link #retrieveImap()} method retrieves all the emails sent to an account whose credentials are configured in
+ * the Mail subsystem using IMAP.
  * </p>
  *
  * @author Joel Tosi
- *
  */
 
 @Named
@@ -56,13 +64,137 @@ public class Email implements Serializable {
     @Resource(mappedName = "java:jboss/mail/MyOtherMail")
     private Session mySession;
 
-    private String to;
+    private String to = "user02@james.local";
 
-    private String from;
+    private String from = "user01@james.local";
 
     private String subject;
 
     private String body;
+
+    private String pop3User = "user02@james.local";
+
+    private String pop3Password = "1234";
+
+    private String pop3Emails;
+
+    private String imapEmails;
+
+    /**
+     * Method to send the email based upon values entered in the JSF view. Exception should be handled in a production usage but
+     * is not handled in this example.
+     */
+    public void send() {
+        try {
+            Message message = new MimeMessage(mySession);
+            message.setFrom(new InternetAddress(from));
+            Address toAddress = new InternetAddress(to);
+            message.addRecipient(Message.RecipientType.TO, toAddress);
+            message.setSubject(subject);
+            message.setContent(body, "text/plain");
+            Transport.send(message);
+
+            FacesContext context = FacesContext.getCurrentInstance();
+            FacesMessage facesMessage = new FacesMessage("Email sent to " + to);
+            context.addMessage(null, facesMessage);
+        } catch (Exception e) {
+            FacesContext context = FacesContext.getCurrentInstance();
+            FacesMessage facesMessage = new FacesMessage("Error sending the Email. " + e.getMessage());
+            context.addMessage(null, facesMessage);
+        }
+    }
+
+    public void resetSmtp() {
+        from = "user01@james.local";
+        to = "user02@james.local";
+        subject = null;
+        body = null;
+    }
+
+    public void retrievePop3() throws Exception {
+        try {
+            pop3Emails = retrieveEmails("pop3", pop3User, pop3Password);
+            if (pop3Emails == null) {
+                FacesContext context = FacesContext.getCurrentInstance();
+                FacesMessage facesMessage = new FacesMessage("No message found.");
+                context.addMessage(null, facesMessage);
+            }
+        } catch (Exception e) {
+            FacesContext context = FacesContext.getCurrentInstance();
+            FacesMessage facesMessage = new FacesMessage("Error retrieving emails using POP3. " + e.getMessage());
+            context.addMessage(null, facesMessage);
+        }
+    }
+
+    public void resetPop3() {
+        pop3User = "user02@james.local";
+        pop3Password = "1234";
+        pop3Emails = null;
+    }
+
+    public void retrieveImap() throws Exception {
+        try {
+            imapEmails = retrieveEmails("imap");
+            if (imapEmails == null) {
+                FacesContext context = FacesContext.getCurrentInstance();
+                FacesMessage facesMessage = new FacesMessage("No message found.");
+                context.addMessage(null, facesMessage);
+            }
+        } catch (Exception e) {
+            FacesContext context = FacesContext.getCurrentInstance();
+            FacesMessage facesMessage = new FacesMessage("Error retrieving emails using IMAP. " + e.getMessage());
+            context.addMessage(null, facesMessage);
+        }
+    }
+
+    public void resetImap() {
+        imapEmails = null;
+    }
+
+    private String retrieveEmails(String protocol) throws MessagingException, IOException {
+        return retrieveEmails(protocol, null, null);
+    }
+
+    private String retrieveEmails(String protocol, String user, String password) throws MessagingException, IOException {
+        Store store = mySession.getStore(protocol);
+        if (user != null && !user.trim().isEmpty()) {
+            store.connect(user, password);
+        } else {
+            // Users the credentials configured in the Mail Subsystem
+            store.connect();
+        }
+
+        Folder inbox = store.getFolder("Inbox");
+        inbox.open(Folder.READ_ONLY);
+
+        // get the list of inbox messages
+        Message[] messages = inbox.getMessages();
+
+        if (messages.length == 0) {
+            return null;
+        }
+
+        StringBuilder sb = new StringBuilder("Emails retrieved via ").append(protocol).append("\n");
+        for (int i = 0; i < messages.length; i++) {
+            // stop after listing ten messages
+            if (i > 10) {
+                inbox.close(true);
+                store.close();
+            }
+
+            sb.append("Message ").append((i + 1)).append("\n");
+            sb.append("From : ").append(messages[i].getFrom()[0]).append("\n");
+            sb.append("Subject : ").append(messages[i].getSubject()).append("\n");
+            sb.append("Body : ").append(messages[i].getContent().toString()).append("\n");
+            sb.append("Sent Date : ").append(messages[i].getSentDate()).append("\n");
+            sb.append("----------------------------------").append("\n");
+        }
+
+        inbox.close(true);
+        store.close();
+
+        return sb.toString();
+    }
 
     public String getTo() {
         return to;
@@ -96,19 +228,35 @@ public class Email implements Serializable {
         this.body = body;
     }
 
-    /**
-     * Method to send the email based upon values entered in the JSF view. Exception should be handled in a production usage but
-     * is not handled in this example.
-     *
-     * @throws Exception
-     */
-    public void send() throws Exception {
-        Message message = new MimeMessage(mySession);
-        message.setFrom(new InternetAddress(from));
-        Address toAddress = new InternetAddress(to);
-        message.addRecipient(Message.RecipientType.TO, toAddress);
-        message.setSubject(subject);
-        message.setContent(body, "text/plain");
-        Transport.send(message);
+    public String getPop3User() {
+        return pop3User;
+    }
+
+    public void setPop3User(String pop3User) {
+        this.pop3User = pop3User;
+    }
+
+    public String getPop3Password() {
+        return pop3Password;
+    }
+
+    public void setPop3Password(String pop3Password) {
+        this.pop3Password = pop3Password;
+    }
+
+    public String getPop3Emails() {
+        return pop3Emails;
+    }
+
+    public void setPop3Emails(String pop3Emails) {
+        this.pop3Emails = pop3Emails;
+    }
+
+    public String getImapEmails() {
+        return imapEmails;
+    }
+
+    public void setImapEmails(String imapEmails) {
+        this.imapEmails = imapEmails;
     }
 }
---
+++ mail/src/test/java/org/jboss/as/quickstarts/mail/MailTestCaseIT.java
@@ -0,0 +1,143 @@
+package org.jboss.as.quickstarts.mail;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class MailTestCaseIT {
+
+    private static final String DEFAULT_SERVER_HOST = "http://localhost:8080/mail";
+
+    private WebDriver driver;
+
+    @Before
+    public void testSetup() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+        String serverHost = System.getenv("SERVER_HOST");
+        if (serverHost == null) {
+            serverHost = System.getProperty("server.host");
+        }
+        if (serverHost == null) {
+            serverHost = DEFAULT_SERVER_HOST;
+        }
+
+        driver.get(serverHost);
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+    }
+
+    @After
+    public void cleanUp() {
+        if (driver != null) {
+            driver.close();
+        }
+    }
+
+    @Test
+    public void a_testSMTP() {
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        WebElement from = driver.findElement(By.id("smtp_from"));
+        WebElement to = driver.findElement(By.id("smtp_to"));
+        WebElement subject = driver.findElement(By.id("smtp_subject"));
+        WebElement body = driver.findElement(By.id("smtp_body"));
+
+        from.clear();
+        from.sendKeys("user01@james.local");
+
+        to.clear();
+        to.sendKeys("user02@james.local");
+
+        subject.clear();
+        subject.sendKeys("This is a test");
+
+        body.clear();
+        body.sendKeys("Hello user02, I've sent an email.");
+
+        WebElement submitButton = driver.findElement(By.id("smtp_send_btn"));
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.xpath("//ul[@id='smtp_messages']/li"));
+        wait.until(d -> message.isDisplayed());
+
+        Assert.assertEquals("Unexpected result messages after sending an email via SMTP.", "Email sent to user02@james.local", message.getText());
+    }
+
+    @Test
+    public void b_retrievePOP3() {
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        WebElement user = driver.findElement(By.id("pop3_user"));
+        WebElement password = driver.findElement(By.id("pop3_password"));
+
+        user.clear();
+        user.sendKeys("user02@james.local");
+
+        password.clear();
+        password.sendKeys("1234");
+
+        WebElement submitButton = driver.findElement(By.id("pop3_get_emails_btn"));
+        submitButton.click();
+
+        wait.until(d -> {
+            try {
+                WebElement emails = driver.findElement(By.id("pop3_emails"));
+                return !emails.getText().isEmpty();
+            } catch (StaleElementReferenceException sere) {
+                return false;
+            }
+        });
+
+        WebElement emails = driver.findElement(By.id("pop3_emails"));
+        Assert.assertTrue("Expected From not found: " + emails.getText(), emails.getText().contains("From : user01@james.local"));
+        Assert.assertTrue("Expected Subject not found: " + emails.getText(), emails.getText().contains("Subject : This is a test"));
+        Assert.assertTrue("Expected Body not found : " + emails.getText(), emails.getText().contains("Body : Hello user02, I've sent an email."));
+    }
+
+
+    @Test
+    public void c_retrieveIMAP() {
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        WebElement submitButton = driver.findElement(By.id("imap_get_emails_btn"));
+        submitButton.click();
+
+        wait.until(d -> {
+            try {
+                WebElement emails = driver.findElement(By.id("imap_emails"));
+                return !emails.getText().isEmpty();
+            } catch (StaleElementReferenceException sere) {
+                return false;
+            }
+        });
+
+        WebElement emails = driver.findElement(By.id("imap_emails"));
+        Assert.assertNotNull("IMAP No messages found.", emails.getText());
+        Assert.assertTrue("Expected email not found.", emails.getText().contains("From : user01@james.local"));
+        Assert.assertTrue("Expected email not found.", emails.getText().contains("Subject : This is a test"));
+        Assert.assertTrue("Expected email not found.", emails.getText().contains("Body : Hello user02, I've sent an email."));
+    }
+}
