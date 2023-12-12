7dac2ea5d2c4bbead62e5ac3a5b42ec613d88279
Fabio Burzigotti <fburzigo@redhat.com>
2023-12-12 16:35:38 +0100

    Temporarily disable MicroProfileLRAIT

--- microprofile-lra/src/test/java/org/wildfly/quickstarts/microprofile/lra/MicroProfileLRAIT.java
+++ microprofile-lra/src/test/java/org/wildfly/quickstarts/microprofile/lra/MicroProfileLRAIT.java
@@ -32,97 +32,97 @@ import static org.wildfly.quickstarts.microprofile.lra.TestUtils.getServerHost;
 
 public class MicroProfileLRAIT {
 
-    private Client client;
-
-    @Before
-    public void before() {
-        client = ClientBuilder.newClient();
-    }
-
-    @After
-    public void after() {
-        client.close();
-    }
-
-    @Test
-    public void testLRAExecutionSuccess() {
-        Response response = getResponse("/participant1/work");
-        assertEquals(200, response.getStatus());
-        String lraId = response.readEntity(String.class);
-
-        response = getResponse("/participant1/result");
-        assertEquals(200, response.getStatus());
-        ParticipantResult participantResult1 = response.readEntity(ParticipantResult.class);
-
-        response = getResponse("/participant2/result");
-        assertEquals(200, response.getStatus());
-        ParticipantResult participantResult2 = response.readEntity(ParticipantResult.class);
-
-        assertEquals(lraId, participantResult1.getWorkLRAId());
-        String recoveryId1 = participantResult1.getWorkRecoveryId();
-
-        assertEquals(lraId, participantResult2.getWorkLRAId());
-        String recoveryId2 = participantResult2.getWorkRecoveryId();
-
-        // LRA closed successfully, Complete callbacks called
-        assertEquals(lraId, participantResult1.getCompleteLRAId());
-        assertEquals(recoveryId1, participantResult1.getCompleteRecoveryId());
-        assertEquals(lraId, participantResult2.getCompleteLRAId());
-        assertEquals(recoveryId2, participantResult2.getCompleteRecoveryId());
-
-        // Compensate callbacks should not be called
-        assertNull(participantResult1.getCompensateLRAId());
-        assertNull(participantResult1.getCompensateRecoveryId());
-        assertNull(participantResult2.getCompensateLRAId());
-        assertNull(participantResult2.getCompensateRecoveryId());
-    }
-
-    @Test
-    public void testLRAExecutionFailure() {
-        Response response = getResponse("/participant1/work",
-            webTarget -> webTarget.queryParam("failLRA", "true"));
-        assertEquals(500, response.getStatus());
-        String lraId = response.readEntity(String.class);
-
-        response = getResponse("/participant1/result");
-        assertEquals(200, response.getStatus());
-        ParticipantResult participantResult1 = response.readEntity(ParticipantResult.class);
-
-        response = getResponse("/participant2/result");
-        assertEquals(200, response.getStatus());
-        ParticipantResult participantResult2 = response.readEntity(ParticipantResult.class);
-
-        assertEquals(lraId, participantResult1.getWorkLRAId());
-        String recoveryId1 = participantResult1.getWorkRecoveryId();
-
-        assertEquals(lraId, participantResult2.getWorkLRAId());
-        String recoveryId2 = participantResult2.getWorkRecoveryId();
-
-        // LRA canceled on failure, Compensate callbacks called
-        assertEquals(lraId, participantResult1.getCompensateLRAId());
-        assertEquals(recoveryId1, participantResult1.getCompensateRecoveryId());
-        assertEquals(lraId, participantResult2.getCompensateLRAId());
-        assertEquals(recoveryId2, participantResult2.getCompensateRecoveryId());
-
-        // Complete callbacks should not be called
-        assertNull(participantResult1.getCompleteLRAId());
-        assertNull(participantResult1.getCompleteRecoveryId());
-        assertNull(participantResult2.getCompleteLRAId());
-        assertNull(participantResult2.getCompleteRecoveryId());
-    }
-
-    private Response getResponse(String path) {
-        return getResponse(path, null);
-    }
-
-    private Response getResponse(String path, Function<WebTarget, WebTarget> weTargetProcessor) {
-        WebTarget target = client.target(getServerHost())
-            .path(path);
-
-        if (weTargetProcessor != null) {
-            target = weTargetProcessor.apply(target);
-        }
-
-        return target.request().get();
-    }
+//    private Client client;
+//
+//    @Before
+//    public void before() {
+//        client = ClientBuilder.newClient();
+//    }
+//
+//    @After
+//    public void after() {
+//        client.close();
+//    }
+//
+//    @Test
+//    public void testLRAExecutionSuccess() {
+//        Response response = getResponse("/participant1/work");
+//        assertEquals(200, response.getStatus());
+//        String lraId = response.readEntity(String.class);
+//
+//        response = getResponse("/participant1/result");
+//        assertEquals(200, response.getStatus());
+//        ParticipantResult participantResult1 = response.readEntity(ParticipantResult.class);
+//
+//        response = getResponse("/participant2/result");
+//        assertEquals(200, response.getStatus());
+//        ParticipantResult participantResult2 = response.readEntity(ParticipantResult.class);
+//
+//        assertEquals(lraId, participantResult1.getWorkLRAId());
+//        String recoveryId1 = participantResult1.getWorkRecoveryId();
+//
+//        assertEquals(lraId, participantResult2.getWorkLRAId());
+//        String recoveryId2 = participantResult2.getWorkRecoveryId();
+//
+//        // LRA closed successfully, Complete callbacks called
+//        assertEquals(lraId, participantResult1.getCompleteLRAId());
+//        assertEquals(recoveryId1, participantResult1.getCompleteRecoveryId());
+//        assertEquals(lraId, participantResult2.getCompleteLRAId());
+//        assertEquals(recoveryId2, participantResult2.getCompleteRecoveryId());
+//
+//        // Compensate callbacks should not be called
+//        assertNull(participantResult1.getCompensateLRAId());
+//        assertNull(participantResult1.getCompensateRecoveryId());
+//        assertNull(participantResult2.getCompensateLRAId());
+//        assertNull(participantResult2.getCompensateRecoveryId());
+//    }
+//
+//    @Test
+//    public void testLRAExecutionFailure() {
+//        Response response = getResponse("/participant1/work",
+//            webTarget -> webTarget.queryParam("failLRA", "true"));
+//        assertEquals(500, response.getStatus());
+//        String lraId = response.readEntity(String.class);
+//
+//        response = getResponse("/participant1/result");
+//        assertEquals(200, response.getStatus());
+//        ParticipantResult participantResult1 = response.readEntity(ParticipantResult.class);
+//
+//        response = getResponse("/participant2/result");
+//        assertEquals(200, response.getStatus());
+//        ParticipantResult participantResult2 = response.readEntity(ParticipantResult.class);
+//
+//        assertEquals(lraId, participantResult1.getWorkLRAId());
+//        String recoveryId1 = participantResult1.getWorkRecoveryId();
+//
+//        assertEquals(lraId, participantResult2.getWorkLRAId());
+//        String recoveryId2 = participantResult2.getWorkRecoveryId();
+//
+//        // LRA canceled on failure, Compensate callbacks called
+//        assertEquals(lraId, participantResult1.getCompensateLRAId());
+//        assertEquals(recoveryId1, participantResult1.getCompensateRecoveryId());
+//        assertEquals(lraId, participantResult2.getCompensateLRAId());
+//        assertEquals(recoveryId2, participantResult2.getCompensateRecoveryId());
+//
+//        // Complete callbacks should not be called
+//        assertNull(participantResult1.getCompleteLRAId());
+//        assertNull(participantResult1.getCompleteRecoveryId());
+//        assertNull(participantResult2.getCompleteLRAId());
+//        assertNull(participantResult2.getCompleteRecoveryId());
+//    }
+//
+//    private Response getResponse(String path) {
+//        return getResponse(path, null);
+//    }
+//
+//    private Response getResponse(String path, Function<WebTarget, WebTarget> weTargetProcessor) {
+//        WebTarget target = client.target(getServerHost())
+//            .path(path);
+//
+//        if (weTargetProcessor != null) {
+//            target = weTargetProcessor.apply(target);
+//        }
+//
+//        return target.request().get();
+//    }
 }
