dc4618439e4df7cc089f80a39d855945be3db028
Emmanuel Hugonnet <ehugonne@redhat.com>
2024-01-03 09:48:33 +0100

    [WFLY-18574]: Get todo-backend working on OpenShift CI.
    * Fixing the path to the postgresql helm charts configuration values.
    * Fixing the path of the todo-backend helm charts.
    
    Issue: https://issues.redhat.com/browse/WFLY-18574
    
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>

--- .ci/openshift-ci/build-root/scripts/qs-overrides/todo-backend/overridable-functions.sh
+++ .ci/openshift-ci/build-root/scripts/qs-overrides/todo-backend/overridable-functions.sh
@@ -11,10 +11,10 @@ function helmInstall() {
     helm_set_arguments="$2"
 
     # TODO https://issues.redhat.com/browse/WFLY-18574 remove this when persistence is working
-    helm_set_arguments="${helm_set_arguments} -f .ci/openshift-ci/build-root/scripts/qs-overrides/todo-backend/ci.yaml"
+    helm_set_arguments="${helm_set_arguments} -f ../.ci/openshift-ci/build-root/scripts/qs-overrides/todo-backend/ci.yaml"
 
     # Don't quote ${helm_set_arguments} as it breaks the command when empty, and seems to work without
-    helm install "${application}" todo-backend-chart/   --wait --timeout="${helm_install_timeout}" ${helm_set_arguments}
+    helm install "${application}" charts/ --wait --timeout="${helm_install_timeout}" ${helm_set_arguments}
     echo "$?"
 }
 
