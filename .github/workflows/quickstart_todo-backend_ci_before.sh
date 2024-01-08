f242631e19d53564d3a2dde08698dfe0ef1b6e92
Emmanuel Hugonnet <ehugonne@redhat.com>
2024-01-08 17:19:49 +0100

    [WFLY-18887]: todo-backend quickstart CI test fails.
    Allowing to pass extra argumnets when starting the server using the
    'EXTRA_RUN_ARGS' parameter.
    
    Issue: https://issues.redhat.com/browse/WFLY-18887
    
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>

--- .github/workflows/quickstart_todo-backend_ci_before.sh
+++ .github/workflows/quickstart_todo-backend_ci_before.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --name todo-backend-db -e POSTGRES_USER=todos -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 postgres
+docker run --name todo-backend-db -e POSTGRES_USER=todos -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -d postgres
