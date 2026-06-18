be3baa5c80371512e45ba5ecd26b8fddc1a0eafd
Manuel Finelli <jfinelli@redhat.com>
2026-06-18 18:24:51 +0100

    [WFLY-20952] Added integration tests to the JTS quickstart

---
+++ jts/application-component-1/src/test/java/org/jboss/as/quickstarts/cmt/jts/BasicRuntimeIT.java
@@ -0,0 +1,195 @@
+/*
+ * Copyright 2026 JBoss by Red Hat.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstarts.cmt.jts;
+
+import static org.jboss.as.controller.client.helpers.ClientConstants.NAME;
+import static org.jboss.as.controller.client.helpers.ClientConstants.OP;
+import static org.jboss.as.controller.client.helpers.ClientConstants.OP_ADDR;
+import static org.jboss.as.controller.client.helpers.ClientConstants.READ_ATTRIBUTE_OPERATION;
+import static org.jboss.as.controller.client.helpers.ClientConstants.READ_CHILDREN_NAMES_OPERATION;
+import static org.jboss.as.controller.client.helpers.ClientConstants.CHILD_TYPE;
+import static org.jboss.as.controller.client.helpers.ClientConstants.RESULT;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.net.InetAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.dmr.ModelNode;
+import org.junit.Test;
+
+public class BasicRuntimeIT {
+
+    private static final String DEFAULT_SERVER_HOST = "http://localhost:8080";
+    private static final String APP_CONTEXT = "/jts-application-component-1";
+    private static final int SERVER2_MGMT_PORT = 10090;
+
+    @Test
+    public void testHTTPEndpointIsAvailable() throws IOException, InterruptedException, URISyntaxException {
+        String serverHost = getServerHost();
+        final HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(serverHost + APP_CONTEXT + "/"))
+                .GET()
+                .build();
+        final HttpClient client = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.ALWAYS)
+                .connectTimeout(Duration.ofMinutes(1))
+                .build();
+        final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, response.statusCode());
+        final String[] bodyLines = response.body().lines().toArray(String[]::new);
+        assertEquals("<meta http-equiv=\"Refresh\" content=\"0; URL=addCustomer.jsf\">", bodyLines[bodyLines.length - 3]);
+    }
+
+    @Test
+    public void testAddCustomer() throws Exception {
+        HttpClient client = createHttpClient();
+        String uniqueName = "TestCustomer_" + System.currentTimeMillis();
+        long baselineMessages = getMessagesAdded();
+
+        HttpResponse<String> response = submitCustomerForm(client, uniqueName);
+        assertEquals(200, response.statusCode());
+        assertTrue(response.body().contains(uniqueName));
+        assertTrue(response.uri().toString().contains("customers.jsf"));
+
+        Thread.sleep(2000);
+        assertEquals(baselineMessages + 1, getMessagesAdded());
+    }
+
+    @Test
+    public void testDuplicateCustomerRollback() throws Exception {
+        HttpClient client = createHttpClient();
+        String uniqueName = "DuplicateTest_" + System.currentTimeMillis();
+
+        HttpResponse<String> response = submitCustomerForm(client, uniqueName);
+        assertEquals(200, response.statusCode());
+        assertTrue(response.body().contains(uniqueName));
+        assertTrue(response.uri().toString().contains("customers.jsf"));
+
+        Thread.sleep(2000);
+        long baselineMessages = getMessagesAdded();
+
+        response = submitCustomerForm(client, uniqueName);
+        assertEquals(200, response.statusCode());
+        assertTrue(response.uri().toString().contains("duplicate.jsf"));
+
+        Thread.sleep(2000);
+        assertEquals(baselineMessages, getMessagesAdded());
+    }
+
+    private String getServerHost() {
+        String host = System.getenv("SERVER_HOST");
+        if (host == null) {
+            host = System.getProperty("server.host");
+        }
+        if (host == null) {
+            host = DEFAULT_SERVER_HOST;
+        }
+        return host;
+    }
+
+    private HttpClient createHttpClient() {
+        return HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.ALWAYS)
+                .cookieHandler(new CookieManager(null, CookiePolicy.ACCEPT_ALL))
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+    }
+
+    private HttpResponse<String> submitCustomerForm(HttpClient client, String name) throws IOException, InterruptedException, URISyntaxException {
+        String serverHost = getServerHost();
+        URI formUri = new URI(serverHost + APP_CONTEXT + "/addCustomer.jsf");
+
+        HttpResponse<String> getResponse = client.send(
+                HttpRequest.newBuilder(formUri).GET().build(),
+                HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, getResponse.statusCode());
+        String body = getResponse.body();
+        // JSF requires the ViewState token from the hidden form field to accept a POST submission
+        Matcher viewStateMatcher = Pattern.compile("name=\"jakarta\\.faces\\.ViewState\"[^>]*value=\"([^\"]+)\"").matcher(body);
+        assertTrue("ViewState not found in response", viewStateMatcher.find());
+        String viewState = viewStateMatcher.group(1);
+
+        HttpRequest postRequest = HttpRequest.newBuilder(formUri)
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(ofFormData(Map.of(
+                        "addCustomerForm", "addCustomerForm",
+                        "addCustomerForm:name", name,
+                        "addCustomerForm:add", "Add",
+                        "jakarta.faces.ViewState", viewState)))
+                .build();
+        return client.send(postRequest, HttpResponse.BodyHandlers.ofString());
+    }
+
+    private long getMessagesAdded() throws IOException {
+        try (ModelControllerClient client = ModelControllerClient.Factory.create(
+                InetAddress.getByName("localhost"), SERVER2_MGMT_PORT)) {
+            String deploymentName = findComponent2Deployment(client);
+            ModelNode op = new ModelNode();
+            op.get(OP).set(READ_ATTRIBUTE_OPERATION);
+            op.get(OP_ADDR).add("deployment", deploymentName)
+                           .add("subsystem", "messaging-activemq")
+                           .add("server", "default")
+                           .add("jms-queue", "jts-quickstart");
+            op.get(NAME).set("messages-added");
+            ModelNode result = client.execute(op);
+            return result.get(RESULT).asLong();
+        }
+    }
+
+    private String findComponent2Deployment(ModelControllerClient client) throws IOException {
+        ModelNode op = new ModelNode();
+        op.get(OP).set(READ_CHILDREN_NAMES_OPERATION);
+        op.get(CHILD_TYPE).set("deployment");
+        ModelNode result = client.execute(op);
+        for (ModelNode entry : result.get(RESULT).asList()) {
+            String name = entry.asString();
+            if (name.startsWith("jts-application-component-2")) {
+                return name;
+            }
+        }
+        throw new RuntimeException("jts-application-component-2 deployment not found on Server 2");
+    }
+
+    private static BodyPublisher ofFormData(Map<String, String> data) {
+        StringBuilder builder = new StringBuilder();
+        for (Map.Entry<String, String> entry : data.entrySet()) {
+            if (builder.length() > 0) {
+                builder.append("&");
+            }
+            builder.append(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8));
+            builder.append("=");
+            builder.append(URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8));
+        }
+        return BodyPublishers.ofString(builder.toString());
+    }
+}
